b861dd2618df519b9bac8989aa1400fd09c05f26
antur84 <andreas.turku@gmail.com>
2015-11-15 19:59:13 +0100

    Started looking into Material design.

--- mobile/src/main/java/com/filreas/gosthlm/activities/MobileBaseActivity.java
+++ mobile/src/main/java/com/filreas/gosthlm/activities/MobileBaseActivity.java
@@ -1,8 +1,8 @@
 package com.filreas.gosthlm.activities;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import com.filreas.gosthlm.database.GoSthlmRepository;
 import com.filreas.gosthlm.database.IGoSthlmRepository;
@@ -13,7 +13,7 @@ import com.filreas.gosthlm.slapi.SLApi;
 import com.filreas.gosthlm.slapi.SLApiKeyFetcher;
 import com.filreas.gosthlm.slapi.SLRestApiClient;
 
-public class MobileBaseActivity extends AppCompatActivity {
+public class MobileBaseActivity extends Activity {
     private MobileClient mobileClient;
     private static final String STATE_RESOLVING_ERROR = "resolving_error";
     protected ISLApi slApi;
