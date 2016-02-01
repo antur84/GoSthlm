edad4f61d6ecc1c16d13818e31f6e1dd2ba3b826
antur84 <andreas.turku@gmail.com>
2016-02-01 18:20:35 +0100

    Removed logging if in release mode.

--- shared/src/main/java/com/filreas/shared/utils/GoSthlmLog.java
+++ shared/src/main/java/com/filreas/shared/utils/GoSthlmLog.java
@@ -2,15 +2,21 @@ package com.filreas.shared.utils;
 
 import android.util.Log;
 
+import com.filreas.shared.BuildConfig;
+
 public class GoSthlmLog {
 
     private static final String gosthlmTag = "gosthlm";
 
     public static void d(String msg) {
-        Log.d(gosthlmTag, msg);
+        if(BuildConfig.DEBUG) {
+            Log.d(gosthlmTag, msg);
+        }
     }
 
     public static void e(Exception e) {
-        Log.e(gosthlmTag, Log.getStackTraceString(e));
+        if(BuildConfig.DEBUG) {
+            Log.e(gosthlmTag, Log.getStackTraceString(e));
+        }
     }
 }
